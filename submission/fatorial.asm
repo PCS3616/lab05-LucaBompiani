7c8f5fd4e0f315f680782a3baa4cf97451af218f
LucaBompiani <luca.bompiani@usp.br>
2025-02-19 18:20:58 -0300

    att7

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -17,11 +17,11 @@ LOOP    LD N        ; Carrega N atual
 
 SET_ONE LV ONE      ; Caso especial N=0
         MM RES      ; Define resultado como 1
+        JP HALT     ; Para execução
 
 HALT    HM =0       ; Para execução
 
-; Área de dados
-@ /0100
+@ /102 
 N       K =0        ; N armazenado no endereço 100
 RES     K =1        ; RES armazenado no endereço 102
 TEMP    K =0        ; Variável temporária
