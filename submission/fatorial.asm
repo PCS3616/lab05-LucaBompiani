c1561dd647d239549aae217ddc6f77b120e16748
LucaBompiani <luca.bompiani@usp.br>
2025-02-19 15:21:58 -0300

    attempt2

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,70 +1,26 @@
-; Função fatorial
-; Entrada: N (armazenado em 0x0100)
-; Saída: RES (armazenado em 0x0102)
-; (Lembre-se: a memória para as variáveis – N, RES e para o stack pointer (0x00FF) – 
-; já é considerada disponível; não use ORG e DW, apenas os endereços)
+START:  LD N           ; Load N value into AC
+        JZ SET_ONE     ; If N is 0, jump to set result to 1
+        LV 1           ; Initialize result with 1
+        MM RES         ; Store initial result
+        
+LOOP:   LD N           ; Load current N value
+        SB ONE         ; Subtract 1 from N
+        MM TEMP        ; Store N-1 in temporary variable
+        LD RES         ; Load current result
+        ML N           ; Multiply by current N value
+        MM RES         ; Update result
+        LD TEMP        ; Load N-1 value
+        MM N           ; Update N to N-1
+        JZ HALT        ; If N reached 0, halt
+        JP LOOP        ; Continue factorial calculation
 
-        LD 0100        ; Carrega N no acumulador
-        JZ f_base      ; Se N == 0, pula para o caso base (retorna 1)
-        SC f_rec       ; Chama a subrotina fatorial recursiva
-        MM 0102       ; Armazena o resultado em RES
-        HM            ; Termina o programa
+SET_ONE: LV 1          ; Handle special case N=0
+        MM RES         ; Set result to 1
 
-f_base:
-        LV 0001       ; Caso base: carrega 1 no acumulador
-        MM 0102       ; Armazena 1 em RES
-        HM            ; Termina
+HALT:   HM             ; Stop execution
 
-; Subrotina fatorial recursiva
-f_rec:
-        ; Empilha o endereço de retorno
-        MM 00FF
-        AD 0001
-        MM 00FF
+ONE:    DW 1           ; Constant value 1
+TEMP:   DW 0           ; Temporary storage for N-1
 
-        ; Empilha o valor de N
-        LD 0100
-        MM 00FF
-        AD 0001
-        MM 00FF
-
-        ; Calcula N - 1 e atualiza N
-        LD 0100
-        SB 0001
-        MM 0100
-
-        ; Chamada recursiva para fatorial(N-1)
-        JZ f_rec_base
-        SC f_rec
-
-        ; Desempilha o valor original de N
-        LD 00FF
-        SB 0001
-        MM 00FF
-        LD 00FF
-
-        ; Multiplica o resultado da chamada recursiva por N
-        ML 0102
-        MM 0102
-
-        ; Desempilha o endereço de retorno e retorna
-        LD 00FF
-        SB 0001
-        MM 00FF
-        RS 00FF
-
-f_rec_base:
-        ; Caso especial da recursão (N-1 == 0)
-        LV 0001
-        MM 0102
-        ; Desempilha o valor original de N
-        LD 00FF
-        SB 0001
-        MM 00FF
-        LD 00FF
-
-        ; Desempilha o endereço de retorno e retorna
-        LD 00FF
-        SB 0001
-        MM 00FF
-        RS 00FF 
+N       EQU 0x100      ; Input number address
+RES     EQU 0x102      ; Result storage address
