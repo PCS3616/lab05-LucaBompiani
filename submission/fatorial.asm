49d00a9437ab3566f9514a0c8c4dd624ffa813cc
LucaBompiani <luca.bompiani@usp.br>
2025-02-19 15:28:56 -0300

    att3

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,26 +1,21 @@
-START:  LD N           ; Load N value into AC
+START:  LD 0100        ; Load N value into AC
         JZ SET_ONE     ; If N is 0, jump to set result to 1
-        LV 1           ; Initialize result with 1
-        MM RES         ; Store initial result
+        LV 0001        ; Initialize result with 1
+        MM 0102        ; Store initial result
         
-LOOP:   LD N           ; Load current N value
-        SB ONE         ; Subtract 1 from N
-        MM TEMP        ; Store N-1 in temporary variable
-        LD RES         ; Load current result
-        ML N           ; Multiply by current N value
-        MM RES         ; Update result
-        LD TEMP        ; Load N-1 value
-        MM N           ; Update N to N-1
+LOOP:   LD 0100           ; Load current N value
+        SB 0001         ; Subtract 1 from N
+        MM 0000        ; Store N-1 in temporary variable
+        LD 0102         ; Load current result
+        ML 0100         ; Multiply by current N value
+        MM 0102         ; Update result
+        LD 0000        ; Load N-1 value
+        MM 0100        ; Update N to N-1
         JZ HALT        ; If N reached 0, halt
         JP LOOP        ; Continue factorial calculation
 
-SET_ONE: LV 1          ; Handle special case N=0
-        MM RES         ; Set result to 1
+SET_ONE: 
+        LV 0001        ; Handle special case N=0
+        MM 0102        ; Set result to 1
 
 HALT:   HM             ; Stop execution
-
-ONE:    DW 1           ; Constant value 1
-TEMP:   DW 0           ; Temporary storage for N-1
-
-N       EQU 0x100      ; Input number address
-RES     EQU 0x102      ; Result storage address
